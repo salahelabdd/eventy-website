9b5120570f6aa5721e6203b9df4a43feec2eeb97
Salah <salahelabd000@gmail.com>
2026-05-23 14:48:33 +0300

    fix: authMiddleware import

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("require('../models/user')");
+const User = require('../models/user');
 
 const protect = async (req, res, next) => {
   let token;
